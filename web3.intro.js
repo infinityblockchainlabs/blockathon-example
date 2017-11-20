f1ae30ad52d7f84149cf17c31285693bd8c45086
toanhn-ibl <admin@admins-MacBook-3.local>
2017-11-20 14:56:30 +0700

    Update host

--- web3.intro.js
+++ web3.intro.js
@@ -3,11 +3,20 @@ var Tx = require('ethereumjs-tx')
 var configs = require('./config')
 
 web3 = new Web3(new Web3.providers.HttpProvider(configs.host));
+console.log(web3)
 console.log(web3.fromWei(123))
 console.log(web3.toWei(123))
 console.log(web3.toHex(123))
-console.log(web3.isAddress('0x0174b4957ec6366aba1747e28b95719ba370796a'))
+console.log(web3.isAddress('0xd09e35386cca7ea21158eca7faae16a476ec5d84'))
+
+web3.version.getNetwork((err, network) => {
+    console.log('Network id: ', network)
+})
 
 web3.eth.getAccounts((error, accounts) => {
-    console.log(accounts)
+    console.log('Accounts: ', accounts)
+})
+
+web3.eth.getBalance('0xd09e35386cca7ea21158eca7faae16a476ec5d84', (error, balance) => {
+    console.log('Balance: ', balance.toString(10))
 })
